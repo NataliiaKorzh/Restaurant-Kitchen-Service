0a111f0c132b4dedddfe6bfca19d71fcec7083a0
Nataliia Korzh <clarioruberia@gmail.com>
2024-03-18 17:33:36 +0200

    add app/urls

---
+++ app/urls.py
@@ -0,0 +1,94 @@
+from django.urls import path
+
+from .views import (
+    index,
+    CookListView,
+    DishListView,
+    DishTypeListView,
+    CookCreateView,
+    CookDetailView,
+    CookUpdateView,
+    CookDeleteView,
+    DishUpdateView,
+    DishDeleteView,
+    DishCreateView,
+    DishDetailView,
+    DishTypeCreateView,
+    DishTypeDeleteView
+)
+
+urlpatterns = [
+    path("", index, name="index"),
+    path(
+        "cooks/",
+        CookListView.as_view(),
+        name="cook-list",
+    ),
+    path(
+        "cooks/<int:pk>/",
+        CookDetailView.as_view(),
+        name="cook-detail"
+    ),
+    path(
+        "cooks/create",
+        CookCreateView.as_view(),
+        name="cook-create",
+    ),
+    path(
+        "cooks/<int:pk>/update/",
+        CookUpdateView.as_view(),
+        name="cook-update",
+    ),
+    path(
+        "cooks/<int:pk>/delete/",
+        CookDeleteView.as_view(),
+        name="cook-delete",
+    ),
+    path(
+        "dishes/",
+        DishListView.as_view(),
+        name="dish-list",
+    ),
+    path(
+        "dishes/create",
+        DishCreateView.as_view(),
+        name="dish-create",
+    ),
+    path(
+        "dishes/<int:pk>/update/",
+        DishUpdateView.as_view(),
+        name="dish-update",
+    ),
+    path(
+        "dishes/<int:pk>/delete/",
+        DishDeleteView.as_view(),
+        name="dish-delete",
+    ),
+    path(
+        "dishes/<int:pk>/",
+        DishDetailView.as_view(),
+        name="dish-detail",
+    ),
+    path(
+        "dish-types/",
+        DishTypeListView.as_view(),
+        name="dish-type-list",
+    ),
+    path(
+        "dish-types/create",
+        DishTypeCreateView.as_view(),
+        name="dish-type-create",
+    ),
+    path(
+        "dish-types/<int:pk>/update/",
+        DishUpdateView.as_view(),
+        name="dish-type-update",
+    ),
+    path(
+        "dish-types/<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete",
+    ),
+    ]
+
+app_name = "app"
